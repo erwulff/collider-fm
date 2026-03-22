8da95a2193bc03de39c262a904561ab34c6045c6
Eric <eric.wulff@cern.ch>
2026-03-22 14:28:56 -0400

    add environment setup script for SLURM jobs

---
+++ slurm/load_env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+module --force purge; module load modules/2.4-20250724
+module load slurm gcc cmake cuda/12.8.0 cudnn/9.2.0.82-12 nccl openmpi apptainer uv
+
+source .venv/bin/activate
+echo "Using Python: $(which python)"
+python --version
+
+echo "Using uv: $(command -v uv)"
+echo "uv version: $(uv --version)"
+echo "uv Python version: $(uv run python --version)"
